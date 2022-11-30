6136dab1576f01d16a8bc72c2b2f4aa453a8a048
Ravi <ravivyas922@gmail.com>
2022-11-30 12:55:36 -0500

    commit tree 4ce53e9156faff556f2063c5a9098da5da5d01c5

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(drivers,str)
+{
+
+    return drivers.filter(driver => driver.toLowerCase() === str.toLowerCase());
+
+}
+function fuzzyMatch(drivers,str)
+{
+    return drivers.filter(driver => driver.startsWith(str));
+}
+function matchName(drivers,str)
+{
+    return drivers.filter(driver => driver.name === str);
+}
