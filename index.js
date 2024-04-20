97954b3ef42f12c0d0a4e0e085b276d2431ce3c4
Ravi <ravivyas922@gmail.com>
2024-04-19 23:55:57 -0400

    Complete Lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(arr,str){
+    const newArr = arr.filter(match => match.toLowerCase() === str.toLowerCase());
+    return newArr;
+}
+
+function fuzzyMatch(arr,str){
+    const newArr = arr.filter(match => match[0] === str[0]);
+    return newArr; 
+}
+
+function matchName(arr,str){
+const newArr = arr.filter(obj => obj.name === str); 
+return newArr; 
+}
